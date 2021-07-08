48eb3416a219b76d7257b57888df139fcb8b4b1a
feras hussain <feras.hussain@PSDSARC.org.sa>
2021-07-08 12:00:41 +0300

    assinment requiemnets

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <section>
+        <div>
+            <active-user
+             v-for="user in users"
+            :key="user.name"
+            :name="user.name"
+            :age="user.age"
+            > </active-user>
+        </div>
+        <div>
+            <user-data
+            @add-user="addUser"
+            > </user-data>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: [
+                {
+                    name: 'knjb',
+                    age: '4',
+                }
+            ]
+        };
+    },
+    methods: {
+        addUser(name, age) {
+            const newUser = {
+                name: name,
+                age: age
+            };
+            this.users.push(newUser);
+        },
+    }
+}
+</script>
---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>
+        <h2> {{ name }} </h2>
+        <h3> {{ age }} </h3>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        age: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+        }
+    },
+}
+</script>
